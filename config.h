28e1729956f18fa5d8163c90491d7c0fcd3bb3a1
mbehboodian <29199390+xeptore@users.noreply.github.com>
2021-07-11 08:08:37 +0430

    config: add config.h

---
+++ config.h
@@ -0,0 +1,7 @@
+const unsigned int BLOCK_THREADS = 500;
+
+const unsigned int GRID_BLOCKS = 24;
+
+const char *INPUT_IMAGE_FILENAME = "input.jpg";
+
+const char *OUTPUT_IMAGE_FILENAME = "output.jpg";
